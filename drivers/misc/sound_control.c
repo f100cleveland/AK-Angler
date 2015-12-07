39d4187100e856fd190488f34b037881351dc480
anarkia1976 <stefano.villa1976@gmail.com>
2015-12-07 11:59:41 +0100

    wcd9330: angler: sound: cleaned all references (spwaker/mic) also into sound driver

--- drivers/misc/sound_control.c
+++ drivers/misc/sound_control.c
@@ -13,8 +13,6 @@
 #include <linux/miscdevice.h>
 
 extern void update_headphones_volume_boost(int vol_boost);
-extern void update_speaker_gain(int vol_boost);
-extern void update_mic_gain(int vol_boost);
 
 /*
  * Volume boost value
@@ -22,20 +20,6 @@ extern void update_mic_gain(int vol_boost);
 int headphones_boost = 0;
 int headphones_boost_limit = 20;
 int headphones_boost_limit_min = -20;
-
-/*
- * Speaker boost value
- */
-int speaker_boost = 0;
-int speaker_boost_limit = 20;
-int speaker_boost_limit_min = -20;
-
-/*
- * Mic boost value
- */
-int mic_boost = 0;
-int mic_boost_limit = 20;
-int mic_boost_limit_min = -20;
 
 /*
  * Sysfs get/set entries
@@ -70,75 +54,12 @@ static ssize_t headphones_boost_store(struct device *dev,
     return size;
 }
 
-static ssize_t speaker_boost_show(struct device *dev, 
-		struct device_attribute *attr, char *buf)
-{
-    return sprintf(buf, "%d\n", speaker_boost);
-}
-
-static ssize_t speaker_boost_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t size)
-{
-    int new_val;
-
-	sscanf(buf, "%d", &new_val);
-
-	if (new_val != speaker_boost) {
-		if (new_val <= speaker_boost_limit_min)
-			new_val = speaker_boost_limit_min;
-
-		else if (new_val >= speaker_boost_limit)
-			new_val = speaker_boost_limit;
-
-		pr_info("New speaker_boost: %d\n", new_val);
-
-		speaker_boost = new_val;
-		update_speaker_gain(speaker_boost);
-	}
-
-    return size;
-}
-
-static ssize_t mic_boost_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-    return sprintf(buf, "%d\n", mic_boost);
-}
-
-static ssize_t mic_boost_store(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t size)
-{
-    int new_val;
-
-	sscanf(buf, "%d", &new_val);
-
-	if (new_val != mic_boost) {
-		if (new_val <= mic_boost_limit_min)
-			new_val = mic_boost_limit_min;
-
-		else if (new_val >= mic_boost_limit)
-			new_val = mic_boost_limit;
-
-		pr_info("New mic_boost: %d\n", new_val);
-
-		mic_boost = new_val;
-		update_mic_gain(mic_boost);
-	}
-
-    return size;
-}
-
 static DEVICE_ATTR(headphones_boost, 0664, headphones_boost_show, 
 	headphones_boost_store);
-static DEVICE_ATTR(speaker_boost, 0664, speaker_boost_show, 
-	speaker_boost_store);
-static DEVICE_ATTR(mic_boost, 0664, mic_boost_show, mic_boost_store);
 
 static struct attribute *soundcontrol_attributes[] =
 {
 	&dev_attr_headphones_boost.attr,
-	&dev_attr_speaker_boost.attr,
-	&dev_attr_mic_boost.attr,
 	NULL
 };
 
