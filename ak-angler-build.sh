574abb6d3e15baca685dc82e6597e3a5b109086c
f100cleveland <dustin.rinne@gmail.com>
2015-12-13 21:23:18 -0600

    build with 5.3 tc

--- ak-angler-build.sh
+++ ak-angler-build.sh
@@ -19,12 +19,12 @@ TOOLCHAIN_DIR="${HOME}/toolchain"
 
 # Kernel Details
 BASE_AK_VER="AK"
-VER=".Fuckery.1.014"
+VER=".Fuckery.1.014-5.3"
 AK_VER="$BASE_AK_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $AK_VER`
-export CROSS_COMPILE="$TOOLCHAIN_DIR/UBERTC-aarch64-linux-android-4.9-kernel/bin/aarch64-linux-android-"
+export CROSS_COMPILE="$TOOLCHAIN_DIR/UBERTC-aarch64-linux-android-5.3-kernel/bin/aarch64-linux-android-"
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER=f100cleveland
